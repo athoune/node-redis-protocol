09db211a0e455baba65446127120e1da6ab58b71
Mathieu Lecarme <mathieu@garambrogne.net>
2012-01-19 23:25:55 +0100

    Cluster async start, get an unique id and a starting job_id counter.

--- examples/triangle/front.js
+++ examples/triangle/front.js
@@ -10,26 +10,27 @@ var redis = require('redis'),
 // cache job result
 // handle missed response.
 
-var cluster = cluster_lib.createCluster();
+var cluster = cluster_lib.createCluster(function() {
+    var web = connect.createServer(
+        connect.favicon(),
 
-var web = connect.createServer(
-    connect.favicon(),
-
-    connect.router(function(app) {
-        app.get('/',function(req, res, next) {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            //put task to the queue.
-            var job_id = cluster.work('working', 'something_long', ['hello', 'world'],
-                cluster.self(), function(err, resp) {
+        connect.router(function(app) {
+            app.get('/',function(req, res, next) {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                //put task to the queue.
+                var job_id = cluster.work('working', 'something_long', ['hello', 'world'],
+                    cluster.self(), function(err, resp) {
+                    });
+                //the answer is back, lets write it.
+                cluster.on('id:something_long:' + job_id, function(arg) {
+                    res.write(arg[0]);
+                    res.end('\n');
                 });
-            //the answer is back, lets write it.
-            cluster.on('id:something_long:' + job_id, function(arg) {
-                res.write(arg[0]);
-                res.end('\n');
             });
-        });
-    })
-);
+        })
+        );
 
-web.listen(1337, "127.0.0.1");
+    web.listen(1337, "127.0.0.1");
+
+});
 
